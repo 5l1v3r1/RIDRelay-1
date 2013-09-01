e8ca4e08cd91697e68b20280ccbadf048ee3cb84
B. Al <berend.al@hva.nl>
2013-09-01 17:25:22 +0000

    added an Output(...) Macro

--- network/Packet.c
+++ network/Packet.c
@@ -53,58 +53,60 @@ static uint32_t readInt(const int source,const int destination)
 	return i;
 }
 
+#define Output(...) Log(LOGT_TUNNEL,LOGL_RESULT,__VA_ARGS__);
+
 void readcsPing(const int client, const int server)
 {
 	(void)client;
 	(void)server;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n> 0x%02x ; PONG",OPC_PING);
+	Output("\n> 0x%02x ; PONG",OPC_PING);
 }
 
 void readcsUpdate(const int client, const int server)
 {
 	(void)client;
 	(void)server;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n> 0x%02x ; update request",OPC_UPDATE);
+	Output("\n> 0x%02x ; update request",OPC_UPDATE);
 }
 
 void readcsGraph(const int client, const int server)
 {
 	uint32_t x;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n> 0x%02x ; graph request",OPC_GRAPH);
+	Output("\n> 0x%02x ; graph request",OPC_GRAPH);
 	x=readInt(client,server);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; name length: %d",x,x);
+	Output("\t0x%08x ; name length: %d",x,x);
 	while(x-->0)
 	{
 		uint8_t c=readByte(client,server);
-		Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; '%c'",c,c);
+		Output("\t0x%08x ; '%c'",c,c);
 	}
 }
 
 void readscLogin(const int server, const int client)
 {
 	int x;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n< 0x%02x ; opcode: Login",OPC_LOGIN);
+	Output("\n< 0x%02x ; opcode: Login",OPC_LOGIN);
 	x=(int)readInt(server,client);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; protocol version: %d",x,x);
+	Output("\t0x%08x ; protocol version: %d",x,x);
 }
 
 void readscPing(const int server, const int client)
 {
 	(void)client;
 	(void)server;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n< 0x%02x ; PING",OPC_PING);
+	Output("\n< 0x%02x ; PING",OPC_PING);
 }
 
 void readscUpdate(const int server,const int client)
 {
 	int x;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n< 0x%02x ; opcode: Update",OPC_UPDATE);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; unittype",readInt(server,client));
+	Output("\n< 0x%02x ; opcode: Update",OPC_UPDATE);
+	Output("\t0x%08x ; unittype",readInt(server,client));
 	x=(int)readInt(server,client);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; amount of sensors: %d",x,x);
+	Output("\t0x%08x ; amount of sensors: %d",x,x);
 	while(x-->0)
 	{
-		Log(LOGT_TUNNEL,LOGL_RESULT,"\t\t0x%08x",readInt(server,client));
+		Output("\t\t0x%08x",readInt(server,client));
 	}
 
 }
@@ -112,36 +114,36 @@ void readscUpdate(const int server,const int client)
 void readscGraph(const int server,const int client)
 {
 	int x;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n< 0x%02x ; opcode: Graph",OPC_GRAPH);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; unit code", readInt(server,client));
+	Output("\n< 0x%02x ; opcode: Graph",OPC_GRAPH);
+	Output("\t0x%08x ; unit code", readInt(server,client));
 	x=(int)readInt(server,client);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; %d datapoints inbound", x,x);
+	Output("\t0x%08x ; %d datapoints inbound", x,x);
 	while(x-->0)
 	{
-		Log(LOGT_TUNNEL,LOGL_RESULT,"\t\t0x%08x", readInt(server, client));
+		Output("\t\t0x%08x", readInt(server, client));
 	}
 	
 }
 
 void readscAlarm(const int server,const int client)
 {
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n< 0x%02x ; opcode: Alarm",OPC_ALARM);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; unit code", readInt(server,client));
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; current sensor value", readInt(server,client));
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; counteraction code",readInt(server,client));
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; sensor number",readInt(server,client));
+	Output("\n< 0x%02x ; opcode: Alarm",OPC_ALARM);
+	Output("\t0x%08x ; unit code", readInt(server,client));
+	Output("\t0x%08x ; current sensor value", readInt(server,client));
+	Output("\t0x%08x ; counteraction code",readInt(server,client));
+	Output("\t0x%08x ; sensor number",readInt(server,client));
 }
 
 void readcsAlarm(const int client, const int server)
 {
 	int x;
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\n> 0x%02x ; opcode: Alarm",OPC_ALARM);
+	Output("\n> 0x%02x ; opcode: Alarm",OPC_ALARM);
 	x=(int)readInt(client, server);
-	Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; namelength: %d", x,x);
+	Output("\t0x%08x ; namelength: %d", x,x);
 	
 	while(x-->0)
 	{
-		Log(LOGT_TUNNEL,LOGL_RESULT,"\t0x%08x ; name", readByte(client, server));
+		Output("\t0x%08x ; name", readByte(client, server));
 	}
 }
 
